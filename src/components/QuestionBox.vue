df2f91deb7d62ae25d73e6c87ca55542b6a6c048
phanvietdung2101 <phanvietdung2101@gmail.com>
2020-07-27 17:43:22 +0700

    fix answer

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,11 +6,16 @@
       </template>
 
       <hr class="my-4" />
-
-      <p v-for="(answer, index) in answers" :key="index">
-        {{ answer }}
-      </p>
-
+      <div>
+        <b-list-group>
+          <b-list-group-item
+            v-for="(answer, index) in answers"
+            :key="index"
+          >
+            {{ answer }}
+          </b-list-group-item>
+        </b-list-group>
+      </div>
       <b-button variant="primary" >Do Something</b-button>
       <b-button variant="success" v-on:click="next">Next question</b-button>
     </b-jumbotron>
@@ -29,6 +34,22 @@ export default {
       answer.push(this.currentQuestion.correct_answer)
       return answer
     }
+  },
+  methods: {
+    selectAnswer (index) {
+      console.log(index)
+    }
+  },
+  mounted () {
+    console.log(this.currentQuestion)
   }
 }
 </script>
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.btn {
+  margin: 0 5px;
+}
+</style>
